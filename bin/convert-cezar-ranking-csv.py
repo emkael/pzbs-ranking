8d8184da5eef77927d89b4380d5764527a5ccb8d
emkael <emkael@tlen.pl>
2017-06-07 02:26:12 +0200

    Helper script for converting Cezar ranking CSV to usable format

---
+++ bin/convert-cezar-ranking-csv.py
@@ -0,0 +1,9 @@
+import csv, sys
+
+data = list(csv.reader(file(sys.argv[1]), delimiter=";"))
+
+output = csv.writer(file(sys.argv[2], 'w'))
+
+date = data[1][3]
+for row in data[4:-1]:
+    output.writerow([row[0], date, row[2], row[1]])
