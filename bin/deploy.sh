b3a1dbdf8e6a93b346932ed673908ba481e21c7a
emkael <emkael@tlen.pl>
2017-06-20 20:20:01 +0200

    Removing Useless Use of Cat

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,7 +2,7 @@
 DEPLOYPATH=`cat config/deploy-path`
 if [ -s config/deploy-pass ]
 then
-    sshpass -p `cat config/deploy-pass` rsync -urpP --delete http/ $DEPLOYPATH
+    sshpass -f config/deploy-pass rsync -urpP --delete http/ $DEPLOYPATH
 else
     rsync -urpP --delete http/ $DEPLOYPATH
 fi
