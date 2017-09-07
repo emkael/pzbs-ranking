7b775200ef44e22d2c5ab0dffce6be5f2ca136fa
emkael <emkael@tlen.pl>
2017-09-07 14:20:09 +0200

    Script for pre-generating menu content

---
+++ scripts/generate-static-menu.py
@@ -0,0 +1,18 @@
+import json, os, sys
+
+from bs4 import BeautifulSoup as bs4
+
+config_file = sys.argv[1]
+base_directory = sys.argv[2]
+menu_directory = sys.argv[3]
+
+for menu_item in json.load(file(config_file)):
+    href = os.path.relpath(
+        os.path.join(base_directory, menu_item['url']),
+        menu_directory
+    )
+    link = bs4('<a class="btn btn-default"></a>', 'html.parser')
+    link.a['href'] = href
+    link.a['title'] = menu_item['header']
+    link.a.string = menu_item['label']
+    print link.a
