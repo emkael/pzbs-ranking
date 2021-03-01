f5cce81e9a745e40909b08c9cf87113035e3f7eb
emkael <emkael@tlen.pl>
2021-03-01 16:21:34 +0100

    Hidden players script adjustments: flag as hidden immediately and ensure CSV import into MySQL

--- bin/hidden-import.sh
+++ bin/hidden-import.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 DBCONFIG=($(jq -r '.[]' config/import-db.json))
-mysql -h ${DBCONFIG[2]} -P ${DBCONFIG[3]} -u ${DBCONFIG[0]} --password=${DBCONFIG[1]} -e "SET foreign_key_checks = 0;
+mysql --local-infile=1 -h ${DBCONFIG[2]} -P ${DBCONFIG[3]} -u ${DBCONFIG[0]} --password=${DBCONFIG[1]} -e "SET foreign_key_checks = 0;
 DELETE FROM hidden_players;
 LOAD DATA LOCAL INFILE '$1'
 INTO TABLE hidden_players
 FIELDS TERMINATED BY ';' ENCLOSED BY '\"';
+UPDATE rankings SET hidden = 1 WHERE pid IN (SELECT pid FROM hidden_players);
 SET foreign_key_checks = 1
 " ${DBCONFIG[4]}
